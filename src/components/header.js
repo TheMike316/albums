cd84005c7e13367b029dd5c31cc296e30f5eb0c7
mikeh <mike.hornegger@gmail.com>
2018-01-07 09:51:35 +0100

    renamed Header.js

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,8 @@ const Header = (props) => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>{props.headerText}</Text> 
-    </View>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View> 
   );
 };
 
